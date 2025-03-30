e6b3b9551ba6f22f5c15104eb3bf8b97c593f3df
Vladislava Diachenko <v.diachenko@ukma.edu.ua>
2025-03-30 13:32:48 +0100

    added functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def read_text_from_console():
+    pass
+
+def read_text_from_file(filepath):
+    pass
+
+def read_text_with_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def print_text_to_console():
+    pass
+
+def write_text_to_file(filepath, text):
+    pass
